4259ae5fe42985d39f23f127d1d3604676d87f88
KutayGuler <s7019@horyzont.eu>
2023-09-01 10:25:07 +0300

    reverted #4c9a87a

--- src/lib/formVerification.ts
+++ src/lib/formVerification.ts
@@ -1,37 +1,36 @@
 // @ts-expect-error
-// prettier-ignore
 export default function formVerification(form) {
-		const value = Object.fromEntries(form.entries());
-		// convert before and after to epoch
-		if (value.before) {
-				value.before = new Date(value.before).getTime() / 1000;
+	const value = Object.fromEntries(form.entries());
+	// convert before and after to epoch
+	if (value.before) {
+		value.before = new Date(value.before).getTime() / 1000;
+	}
+	if (value.after) {
+		value.after = new Date(value.after).getTime() / 1000;
+	}
+	// convert checkboxes to bools
+	for (const key in value) {
+		if (value[key] === 'on') {
+			value[key] = true;
 		}
-		if (value.after) {
-				value.after = new Date(value.after).getTime() / 1000;
+	}
+	// convert empty strings to null
+	for (const key in value) {
+		if (value[key] === '') {
+			delete value[key];
 		}
-		// convert checkboxes to bools
-		for (const key in value) {
-				if (value[key] === 'on') {
-						value[key] = true;
-				}
-		}
-		// convert empty strings to null
-		for (const key in value) {
-				if (value[key] === '') {
-						delete value[key];
-				}
-		}
-		//combine num_comments and num_comments_comparator
-		if (value.num_comments && value.num_comments_comparator) {
-				value.num_comments = `${value.num_comments_comparator}${value.num_comments}`;
-		}
-		//combine score and score_comparator
-		if (value.score && value.score_comparator) {
-				value.score = `${value.score_comparator}${value.score}`;
-		}
-		//remove score_comparator and num_comments_comparator
-		delete value.score_comparator;
-		delete value.num_comments_comparator;
-		// delete value.type;
-		return value;
+	}
+	//combine num_comments and num_comments_comparator
+	if (value.num_comments && value.num_comments_comparator) {
+		value.num_comments = `${value.num_comments_comparator}${value.num_comments}`;
+	}
+	//combine score and score_comparator
+	if (value.score && value.score_comparator) {
+		value.score = `${value.score_comparator}${value.score}`;
+	}
+	//remove score_comparator and num_comments_comparator
+	delete value.score_comparator;
+	delete value.num_comments_comparator;
+	// delete value.type;
+	return value;
 }
