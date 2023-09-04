4fab4753b69653cdc5d76649aa1b32d7a5a47167
KutayGuler <s7019@horyzont.eu>
2023-09-04 12:27:56 +0300

    added rule to markdown-it

--- src/lib/Markdown.svelte
+++ src/lib/Markdown.svelte
@@ -2,11 +2,35 @@
 	import { decodeHTML } from 'entities';
 	import MarkdownIt from 'markdown-it';
 	const md = new MarkdownIt();
-	md.disable(['image']);
+	const alphanumericRegex = /[a-zA-Z0-9]/;
+	md.renderer.rules.image = (tokens, idx, options, env, slf) => {
+		const token = tokens[idx];
+		let src = token.attrGet('src');
+		if (src?.match(alphanumericRegex)) {
+			token.tag = 'p';
+			token.type = 'paragraph_open';
+			token.nesting = 1;
+			token.children = [
+				// @ts-expect-error
+				{
+					attrs: null,
+					block: true,
+					children: null,
+					content: 'text',
+					hidden: false,
+					map: null,
+					markup: 'specific-stuff',
+					meta: null,
+					nesting: 0,
+					tag: '',
+					type: 'text'
+				}
+			];
+		}
+
+		return slf.renderToken(tokens, idx, options);
+	};
 	export let source: string;
-	if (source.includes('Empress Medusa')) {
-		console.log(source);
-	}
 	source = decodeHTML(source);
 	source = md.render(source);
 	source = decodeHTML(source);
