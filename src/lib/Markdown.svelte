a18a893c6f05d0cbbe2f93fa5903b3627e58fa68
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2025-03-03 16:34:56 +0400

    Add image rendering and CSS styles

--- src/lib/Markdown.svelte
+++ src/lib/Markdown.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import { decodeHTML } from 'entities';
 	import MarkdownIt from 'markdown-it';
 	import { highlights } from './stores';
-	import { higlight } from './utils';
-	import xss from 'xss'
-	const md = new MarkdownIt();
+	import { higlight, replaceRedditLinksWithImages, sanitizeHtml } from './utils';
+	import { friendlyAttrValue } from 'xss';
+	const md = new MarkdownIt({
+		html: true,
+	});
 	const alphanumericRegex = /[a-zA-Z0-9]/;
 	md.renderer.rules.image = (tokens, idx, options, env, slf) => {
 		const token = tokens[idx];
@@ -21,40 +22,91 @@
 
 		return slf.renderToken(tokens, idx, options);
 	};
+	md.renderer.rules.code_inline = (tokens, idx, options, env, slf) => {
+		const token = tokens[idx];
+		let text = friendlyAttrValue(token.content);
+		return `<code>${text}</code>`;
+	};
 	export let source: string;
-	source = decodeHTML(source);
-	source = md.render(source);
-	source = decodeHTML(source);
-
-	let content = xss(source, {
-		whiteList: {
-			pre: [],
-			code: [],
-			table: [],
-			blockquote: [],
-			br: [],
-			p: [],
-			li: [],
-			ol: [],
-			ul: [],
-			h1: [],
-			h2: [],
-			h3: [],
-			h4: [],
-			h5: [],
-			h6: [],
-			b: [],
-			strong: [],
-			i: [],
-			em: [],
-			s: [],
-			strike: [],
-			img: ['src', 'alt', 'title'],
-			a: ['href', 'title'],
-			sup: [],
-			hr: [],
-		},
-	})
+	$: renderedMarkdown = md.render(source);
+	$: content = replaceRedditLinksWithImages(sanitizeHtml(renderedMarkdown))
 </script>
 
-{@html higlight(content, $highlights)}
+<div class="container">
+	{@html higlight(content, $highlights)}
+</div>
+
+<style>
+	.container {
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+	}
+
+	:global(.container h1) {
+		font-size: 20px;
+		line-height: 20px;
+		margin-bottom: 10px;
+		font-weight: 400 !important;
+	}
+
+	:global(.container p) {
+		font-size: 14px;
+		line-height: 20px;
+  	line-height: 1.25rem;
+		margin-top: 14px;
+		margin-bottom: 16px;
+	}
+
+	:global(.container code) {
+		border-radius: 4px;
+		border: 1px solid #1b1e20;
+		padding: 2px 4px;
+		font-size: 12.6px;
+		font-family: Noto Mono, Menlo, Monaco, Consolas, monospace;
+	}
+
+	:global(.container ul, .container.ol) {
+		margin: 16px 0;
+		margin-inline-start: 0px;
+		margin-inline-end: 0px;
+		margin-block-start: 14px;
+		margin-block-end: 16px;
+		padding-inline-start: 32px;
+		padding-left: 32px;
+		font-size: 14px;
+	}
+
+	:global(.container ul) {
+		list-style: disc !important;
+	}
+
+	:global(.container ol) {
+		list-style: decimal !important;
+	}
+
+	:global(.container a) {
+		color: #ad8bfe;
+		text-decoration: underline;
+	}
+
+	:global(.container a:hover) {
+		color: #d0c0fe;
+	}
+
+	:global(.container img) {
+		width: fit-content;
+		max-width: 100%;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+	:global(.container figure) {
+		margin-bottom: 16px;
+	}
+
+	:global(.container figcaption) {
+		font-size: 11.2px;
+		color: rgb(187, 195, 203);
+		text-align: center;
+		margin-top: 4.48px;
+	}
+</style>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import xss from 'xss'
+
 export function higlight(source: string, highlights: Array<string>) {
 	if (highlights.length == 0) return source;
 	if (!source) return;
@@ -11,4 +13,63 @@ export function higlight(source: string, highlights: Array<string>) {
 	}
 
 	return source;
+}
+
+const zeroWidthSpaceHtmlEntity = '&amp;#x200B;'
+export function sanitizeHtml(html: string) {
+	return xss(html, {
+		whiteList: {
+			pre: [],
+			code: [],
+			table: [],
+			blockquote: [],
+			br: [],
+			p: [],
+			li: [],
+			ol: [],
+			ul: [],
+			h1: [],
+			h2: [],
+			h3: [],
+			h4: [],
+			h5: [],
+			h6: [],
+			b: [],
+			strong: [],
+			i: [],
+			em: [],
+			s: [],
+			strike: [],
+			img: ['src', 'alt', 'title'],
+			a: ['href', 'title'],
+			sup: [],
+			hr: [],
+		},
+	}).replaceAll(zeroWidthSpaceHtmlEntity, '')
+}
+
+export const replaceRedditLinksWithImages = (html: string) => {
+	const domParser = new DOMParser()
+	const dom = domParser.parseFromString(html, 'text/html')
+	dom.body.querySelectorAll('a').forEach(a => {
+		try {
+			const href = new URL(a.href)
+			if (href.hostname === 'i.redd.it' || href.hostname === 'i.imgur.com' || href.hostname === 'preview.redd.it') {
+				const fig = document.createElement('figure')
+				const img = document.createElement('img')
+				const caption = document.createElement('figcaption')
+				img.src = a.href
+				if (a.textContent !== null) {
+					img.alt = a.textContent
+				}
+				caption.textContent = a.textContent
+				fig.appendChild(img)
+				fig.appendChild(caption)
+				a.replaceWith(fig)
+			}
+		} catch {
+			// ignore
+		}
+	})
+	return dom.body.innerHTML
 }
