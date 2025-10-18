7b14ea12b6a0007a9e774ef49f8fe942bc00b8dc
pullpush-io <135697595+pullpush-io@users.noreply.github.com>
2025-10-18 11:04:09 +0100

    rm: debug dump

--- src/lib/userSection.svelte
+++ src/lib/userSection.svelte
@@ -76,10 +76,6 @@
 				</div>
 			</a>
 			{#key [currentPie, numberType]}
-				{@html (() => {
-					console.log('Rendering pie with data:', currentPie, pieData[currentPie]);
-					return '';
-				})()}
 				<PieChart data={pieData[currentPie]} {numberType}>
 					{#if pieData[currentPie].length > 0}
 						<select class="select rounded-3xl" bind:value={currentPie}>
